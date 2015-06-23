188af96bcab17f9a93da9d50ba6239f1c6b050f3
Karel Vervaeke <karel@ngdata.com>
2015-06-23 22:54:58 +0200

    Fetch by 'nummer' metatag
    There are fewer 'nummer' values which have > 1000 results,
    resulting in less documents missed because of the 1000 results
    restriction

--- vlapar/example.py
+++ vlapar/example.py
@@ -4,5 +4,5 @@ import os
 def eg1():
     if not os.path.exists('cache'):
         print "cache dir does not exist. Did you run 'vlapar getmeta' first?"
-    schv = vlapar.meta.load_meta('SCHV')
-    print schv.groupby(['legislatuur','minister','status']).count()
+    schv = vlapar.meta.MetaFetcher("cache").load_meta('soort','SCHV')
+    print schv.groupby(['legislatuur','minister','status'])['status'].count()
--- vlapar/meta.py
+++ vlapar/meta.py
@@ -5,60 +5,80 @@ import json
 import pandas as pd
 import glob
 
+MAX_PAGE_COUNT=10
+MAX_PAGE_SIZE=100
+
 #FIXME: this list is incomplete
-types = [ 'PI', 'VI', 'JLN', 'SCHV' ]
-
-def download_meta(doctype, start_page=1):
-    """ Download metadata for a given document type (PI, VI, JLN, SCHV)
-    BUGS: the service seems to be limited to 1000 results (10 pages of 100). Create a drill down mechanism
-    which starts over with additional filters to break up queries into manageable parts.  To do this we can include
-    additional filters based on observed metatag values
-    """
-
-    if not os.path.exists('cache'):
-        os.makedirs('cache')
-
-    for page in range(start_page,50):
-        print "Fetching %s page %d"%(doctype,page,)
-        resp = requests.get('http://ws.vlpar.be/e/gsa/resource/search/query/inmeta%%3Asoort%%3D%s?page=%d&max=100'%(doctype,page,))
-        #print resp.text
-        # Stop when we get empty results
-        if 'result' not in resp.json() or len(resp.json()['result']) == 0:
-            return
-
-        with codecs.open("cache/%s-%03d.json"%(doctype,page),'w','utf-8') as f:
-            f.write(resp.text)
+types = [ 'PI', 'VI', 'JLN', 'SCHV', 'DBT' ]
 
 def download_all():
-    """ Download all metadata. Subject to the limitation documented in get_meta_for_type.  """
+    """ Fetches different document types (maximum 1000 per type """
 
-    for doctype in types:
-        download_meta(doctype)
+    MetaFetcher('cache').fetch('soort',types)
 
-def load_meta(doctype):
-    """ Loads metadata for given type as dataframe (type = PI, VI, ...) """
+class MetaFetcher:
+    def __init__(self, cachedir):
+        self.cachedir = cachedir
+        if not os.path.exists(self.cachedir):
+            os.makedirs(self.cachedir)
+        pass
 
-    # TODO: allow customising tags & fields to include in dataframe? (predefined sets? load everything?)
-    tags = ['legislatuur','minister','vraagsteller','status']
-    return _get_dataframe(glob.glob('cache/%s-*.json'%(doctype)), tags)
+    def fetch(self, metatag, values):
+        """ Tries to retrieve all documents matching start_criteria."""
 
-def _get_dataframe(paths, metatags):
-    """ Reads a path or buffer and converts it into a dataframe ready for analysis """
-    all_rows = []
-    for path in paths:
-        obj = json.load(open(path))
-        rows = obj['result']
-        all_rows += [ _record_to_row(record, metatags) for record in rows ]
-        
-    return pd.DataFrame(all_rows, columns=metatags)
+        limit = MAX_PAGE_SIZE * MAX_PAGE_COUNT
 
-def _record_to_row(record, metatags):
-    """ Converts a record into a row for our dataframe """
+        for value in values:
+            # TODO: instead of fetching one pgae beforehand
+            # we could simply check if the last page haad MAX_PAGE_SIZE results.
+            # (more accurate, but we can only warn at the end instead of at the beginning
+            response = self.fetch_page(metatag, value, 0)
 
-    # Get metatags as dict
-    # If key occurs more than once earlier ones are dropped
-    tags = dict([(meta['name'],meta['value']) for meta in record['metatags']['metatag']])
+            count = 0 if response['count'] == '' else int(response['count'])
+            if count > limit:
+                print "Warning: %s:%s yields > 1000 results (%s)"%(metatag,value,response['count'])
 
-    return tuple([tags.get(tag, None) for tag in metatags])
+            if count == 0: continue
 
+            for page in range(1,10):
+                response = self.fetch_page(metatag, value, page)
+                count = 0 if response['count'] == '' else int(response['count'])
+                if len(response['result']) < MAX_PAGE_SIZE:
+                    # We've hit the last page
+                    break
+
+    def fetch_page(self, metatag, value, page):
+        url_format = "http://ws.vlpar.be/e/gsa/resource/search/query/inmeta%%3A%s%%3D%s?page=%d&max=%d"
+        url = url_format%(metatag,value,page,MAX_PAGE_SIZE)
+        resp = requests.get(url)
+        with codecs.open(os.path.join(self.cachedir,"%s:%s-%02d.json"%(metatag,value,page)),'w','utf-8') as f:
+            f.write(resp.text)
+        return resp.json()
+
+    def load_meta(self, metatag, value):
+        """ Loads metadata for given type as dataframe (type = PI, VI, ...) """
+
+        # TODO: allow customising tags & fields to include in dataframe? (predefined sets? load everything?)
+        tags = ['legislatuur','minister','vraagsteller','status','soort','datum','thema','company','source','word count']
+        files = glob.glob(os.path.join(self.cachedir, "%s:%s-*.json"%(metatag,value)))
+        return self._get_metatag_dataframe(files, tags)
+
+    def _get_metatag_dataframe(self, paths, metatags):
+        """ Reads a path or buffer and converts it into a dataframe ready for analysis """
+        all_rows = []
+        for path in paths:
+            obj = json.load(open(path))
+            rows = obj['result']
+            all_rows += [ self._record_to_row(record, metatags) for record in rows ]
+            
+        return pd.DataFrame(all_rows, columns=metatags)
+
+    def _record_to_row(self, record, metatags):
+        """ Converts a record into a row for our dataframe """
+
+        # Get metatags as dict
+        # If key occurs more than once earlier ones are dropped
+        tags = dict([(meta['name'],meta['value']) for meta in record['metatags']['metatag']])
+
+        return tuple([tags.get(tag, None) for tag in metatags])
 
